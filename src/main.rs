9d56ee85b8bcc8ef1796666d825d568cca9846ee
异月 <twhice@outlook.com>
2024-02-02 12:24:26 +0800

    解决了不能运行的问题（还真就是burn的bug(https://github.com/tracel-ai/burn/pull/1225)）
    让rust-analyzer下也有补全了，但是很奇怪（

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use burn::backend::autodiff::grads::Gradients;
 use burn::backend::{wgpu::WgpuDevice, Autodiff, Fusion, Wgpu};
-use burn::tensor::{Distribution, Float, Tensor};
+
+use burn::tensor::{Distribution, Float, Tensor, TensorKind};
 
 type Backend = Autodiff<Fusion<Wgpu>>;
 
@@ -10,30 +11,48 @@ const NUM_DATA: usize = 1024;
 const BATCH_SIZE: usize = 128;
 const DEVICE: WgpuDevice = WgpuDevice::BestAvailable;
 
+trait TensorClone
+where
+    Self: Sized,
+{
+    fn tclone(&self) -> Self;
+}
+
+impl<B, const D: usize, K> TensorClone for Tensor<B, D, K>
+where
+    B: burn::tensor::backend::Backend,
+    K: TensorKind<B>,
+    Self: Sized,
+{
+    fn tclone(&self) -> Self {
+        <Self as Clone>::clone(self)
+    }
+}
+
 fn main() {
     let real_w = WgpuTensor::from_data([2.0, -3.4], &DEVICE).reshape([2, 1]);
     let real_b = WgpuTensor::from_data([[4.2]], &DEVICE);
 
-    let (features, labels): (WgpuTensor, WgpuTensor) = {
+    let (features, labels) = {
         let x = WgpuTensor::random(
             [NUM_DATA, real_w.dims()[0]],
             Distribution::Normal(0.0, 1.0),
             &DEVICE,
         );
-        let y = x.clone().matmul(real_w.clone()) + real_b.clone();
-        let y = y.clone() + y.random_like(Distribution::Normal(0.0, 0.01));
+        let y = x.tclone().matmul(real_w.tclone()) + real_b.tclone();
+        let y = y.tclone() + y.random_like(Distribution::Normal(0.0, 0.01));
 
         (x, y.reshape([-1, 1]))
     };
 
     let mut next = {
         let mut counter = 0;
-        let features = features.clone();
-        let labels = labels.clone();
+        let features = features.tclone();
+        let labels = labels.tclone();
         move || {
             let r = (
-                features.clone().slice([counter..counter + 1, 0..2]),
-                labels.clone().slice([counter..counter + 1, 0..1]),
+                features.tclone().slice([counter..counter + 1, 0..2]),
+                labels.tclone().slice([counter..counter + 1, 0..1]),
             );
             counter += 1;
             counter %= NUM_DATA;
@@ -47,7 +66,7 @@ fn main() {
 
     for counter in 0..128 {
         for (x, y) in (0..BATCH_SIZE).map(|_| next()) {
-            let grad = squared_loss(y, linreg(x, w.clone(), b.clone()))
+            let grad = squared_loss(y, linreg(x, w.tclone(), b.tclone()))
                 .sum()
                 .backward();
             sdg([&mut w, &mut b], grad, lr);
@@ -56,13 +75,13 @@ fn main() {
         println!(
             "第{counter}次 : {}, w : {:?}, b : {}",
             squared_loss(
-                labels.clone(),
-                linreg(features.clone(), w.clone(), b.clone()),
+                labels.tclone(),
+                linreg(features.tclone(), w.tclone(), b.tclone()),
             )
             .mean()
             .into_scalar(),
-            w.clone().into_data().value,
-            b.clone().into_scalar()
+            w.tclone().into_data().value,
+            b.tclone().into_scalar()
         )
     }
 }
@@ -74,7 +93,7 @@ fn linreg(x: WgpuTensor, w: WgpuTensor, b: WgpuTensor) -> WgpuTensor {
 
 #[inline]
 fn squared_loss(y: WgpuTensor, y_hat: WgpuTensor) -> WgpuTensor {
-    (y - y_hat).powf(WgpuTensor::from_data([[2.0]], &WgpuDevice::BestAvailable)) / 2.0
+    (y - y_hat).powi_scalar(2) / 2.0
 }
 
 #[inline]
@@ -85,7 +104,7 @@ fn sdg<const C: usize>(params: [&mut WgpuTensor; C], mut grad: Gradients, lr: f6
                 / 8;
 
         *param = param
-            .clone()
+            .tclone()
             .set_require_grad(false)
             .sub(delta_param)
             .require_grad();
